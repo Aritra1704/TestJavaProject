5c3c3892ee63bc094e55393ba59e4ec40d63f4c6
Aritra1704 <aritra1704@gmail.com>
2019-08-16 00:14:58 +0530

    Create Spiral matrix from a given string.

--- src/devengers/SpiralPatternPrinting.java
+++ src/devengers/SpiralPatternPrinting.java
@@ -10,65 +10,50 @@ public class SpiralPatternPrinting {
 		for(int i = 0; i < (t); i++) {
 			int m = s.nextInt();
 			int n = s.nextInt();
-			int l = s.nextInt();
-			
+			int l = s.nextInt();			
 			String w = s.next();
-//			System.out.println(m + " " + n + " " + l + " " + w);
-			
-			createMatrix(m, n, l, w);
+			cM(m, n, l, w);
 		}
 	}
-
-	private static void createMatrix(int m, int n, int l, String w) {
+	private static void cM(int m, int n, int l, String w){
 		String[][] mat = new String[m][n];
 		int i=0, j=0,k;
 		while(i<m && j<n){
 			for(k=j;k<n;++k){
-				mat[i][k]=getNextChar(l,w);
+				mat[i][k]=gN(l,w);
 			}
 			i++;
 			for(k=i;k<m;++k){
-				mat[k][n - 1]=getNextChar(l,w);
+				mat[k][n - 1]=gN(l,w);
 			}
 			n--;
 			if(i<m){
-				for(k=n-1;k>=i;--k){
-					mat[m-1][k]=getNextChar(l,w);
+				for(k=n-1;k>=j;--k){
+					mat[m-1][k]=gN(l,w);
 				}
 				m--;
 			}
 			if(j<n){
 				for(k=m-1;k>=i;--k){
-					mat[k][j]=getNextChar(l,w);
+					mat[k][j]=gN(l,w);
 				}
 				j++;
 			}
 		}
-		printM(m,n,mat);
-		count=0;
+		pM(mat.length,mat[0].length,mat);
+		c=0;
 	}
-	private static void printM(int m,int n,String[][] mat) {
-		System.out.println("------------------");
-//		int j=0;
+	private static void pM(int m,int n,String[][] mat){
 		for(int i=0;i<m;i++){
 			for(int j=0;j<n;j++){
 				System.out.print(mat[i][j]);
 			}
-//			System.out.println("\n");
+			System.out.print("\n");
 		}
-//		for(int i=0;i<m;j++){
-//			System.out.print(mat[i][j]);
-//			if((j+1)>=n){
-//				i++;
-//				j=0;
-//				System.out.println("\n");
-//			}
-//		}
 	}
-	static int count = 0;
-	private static String getNextChar(int l,String w) {
-		if(count>=l)
-			count=0;
-		return (w.charAt(count++) + "");
+	static int c = 0;
+	private static String gN(int l,String w){
+		if(c>=l) c=0;
+		return (w.charAt(c++) + "");
 	}
 }
